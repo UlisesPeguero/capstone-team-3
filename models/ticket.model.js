3e13beb573e56b3bbff5c107683250693e1bbfb0
UlisesPeguero <peguero.ulises@gmail.com>
2021-02-15 02:25:53 -0600

    Dashboard added

--- models/ticket.model.js
+++ models/ticket.model.js
@@ -2,10 +2,49 @@ const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 
 let Ticket = new mongoose.Schema({
-    description: {
-        type: String
+    number: {
+        type: String,
+        required: false
+    },
+    requestor: {
+        type: String, 
+        required: true
+    },
+    email: {
+        type: String, 
+        required: true
+    },
+    details: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    address2: String,
+    priority: {
+        type: String,
+        required: true,
+        default: 'Low'        
+    },
+    status: {
+        type: String,
+        required: true,
+        default: 'Pending'        
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+        default: new Date()
+    },
+    updatedAt: {
+        type: Date, 
+        required: true,
+        default: new Date()
     }
 });
+
 
 module.exports = mongoose.model(
     'Ticket',   // identifier
